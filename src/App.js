c39d24d49a02eb8f79d095ec7568792274054247
Will-Peterson <william.from.ca@gmail.com>
2021-06-11 23:25:18 -0700

    added accessibility

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import {nanoid} from 'nanoid';
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -16,6 +24,8 @@ function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState('All');
+
+  const listHeadingRef = useRef(null);
 
   function deleteTask(id) {
     const remainingTasks = tasks.filter(task => id !== task.id);
@@ -71,6 +81,14 @@ function App(props) {
   const tasksNoun = taskList.length !==1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -78,7 +96,7 @@ function App(props) {
       <div className="filters btn-group stack-exception">
         {filterList}
       </div>
-      <h2 id="list-heading">
+      <h2 id="list-heading" tabIndex='-1' ref={listHeadingRef}>
         {headingText}
       </h2>
       <ul
